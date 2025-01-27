27c556c9e556032c0346c7b45e73597976ea7723
fabrice48 <niyoyigenerafabrice2@gmail.com>
2025-01-27 05:12:33 -1000

    Add files via upload

--- index.js
+++ index.js
@@ -1,44 +1,41 @@
-const express=require('express');
-const app= express();
-const port=3000;
- const mysql=require('mysql');
-const connection=require(`./database/db`);
+const express = require('express');
+const app = express();
+const port = 3000;
+const connection = require('./database/db.js');
+const mysql = require('mysql');
+// const userRoute=require('./router/user');
+
+// json method
 
 app.use(express.json());
 
-app.get('/users',(reg,res)=>{
-    const sql='select * from users';
-    connection.query(sql,(err,result)=>{
-        if(err)err;
+// get all users
+
+app.get('/users', (req, res) => {
+    const sql = 'select * from api';
+    connection.query(sql, (err, result) => {
+        if (err) err;
         res.send(result);
     })
-    
 });
 
-app.get('/users/:id',(reg,res)=>{
-    const sql='select * from users where id =?';
-    connection.query(sql,(err,result)=>{
-        if(err)err;
-        res.send(result);
-    })
-    
-});
+// insert using post method
 
 
 app.post('/users', (req, res) => {
     const user = req.body;
-    const sql = 'insert into users set ?';
+    const sql = 'insert into api set ?';
     connection.query(sql, user, (err, result) => {
         if (err) err;
         res.send('insert successful');
     });
 });
 
-
+// delete by id
 
 app.delete('/users/:id', (req, res) => {
     const to = req.params.id;
-    const sql = 'delete from users where id=?';
+    const sql = 'delete from api where id=?';
     connection.query(sql, to, (err, result) => {
 
         if (err) err;
@@ -46,12 +43,34 @@ app.delete('/users/:id', (req, res) => {
     })
 });
 
+// get by id
+
+app.get('/users/:id', (req, res) => {
+    const ti = req.params.id;
+    const sql = 'select * from api where id=? ';
+    connection.query(sql, ti, (err, result) => {
+
+        if (err) {
+            res.send(err, 'record not fund');
+        } else {
+            res.send(result);
+        }
+    })
+});
+// update using put method
+app.put('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const name = req.body;
+    const sql = 'update api set id= ?';
+    connection.query(sql, [id, name], (err, result) => {
+        if (err) err;
+        res.send('update successful');
+    });
+});
+
+// listen
 
 
-
-
-
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`server is running on port ${port}`);
-
 });
---
+++ readme.md
@@ -0,0 +1,3 @@
+Name Of Group:
+1.NIYOYATEGETSE Sarah
+2.MANZI Patience
